9497ac3db543020868b1bb61604ce74482be7488
Flash <42217856+caicedos@users.noreply.github.com>
2019-10-23 23:20:33 -0400

    signin/register

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Signin = () => {
     return (
         <div>
-            <main className="mw5 black-80 v-mid shadow-5">
+            <main className="pa4 black-80 shadow-5">
                 <form className="measure center">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f4 fw6 ph0 mh0">Sign In</legend>
@@ -11,23 +12,23 @@ const Signin = () => {
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
                         </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
-                            </div>
-                                <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                        </div>
+                        <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
                     </fieldset>
-                                <div className="">
-                                    <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
-                                </div>
-                                    <div className="lh-copy mt3">
-                                        <a href="#0" className="f6 link dim black db">Sign up</a>
-                                        <a href="#0" className="f6 link dim black db">Forgot your password?</a>
-                                    </div>
+                    <div className="">
+                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                    </div>
+                    <div className="lh-copy mt3">
+                        <Link to='Register' href="#0" className="f6 link dim black db">Register</Link>
+                        <a href="#0" className="f6 link dim black db">Forgot your password?</a>
+                    </div>
                 </form>
             </main>
         </div>
-                        )
-                    }
-                    
+    )
+}
+
 export default Signin;
